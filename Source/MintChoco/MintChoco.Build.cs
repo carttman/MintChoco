b0e4bfeaded0f8a21ae2ec095afbc68fc59ab7bb
pulnip <choiwh1008@gmail.com>
2026-08-29 10:05:16 +0900

    add ue third person template

---
+++ Source/MintChoco/MintChoco.Build.cs
@@ -0,0 +1,51 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+
+public class MintChoco : ModuleRules
+{
+	public MintChoco(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+
+		PublicDependencyModuleNames.AddRange(new string[] {
+			"Core",
+			"CoreUObject",
+			"Engine",
+			"InputCore",
+			"EnhancedInput",
+			"AIModule",
+			"StateTreeModule",
+			"GameplayStateTreeModule",
+			"UMG",
+			"Slate"
+		});
+
+		PrivateDependencyModuleNames.AddRange(new string[] { });
+
+		PublicIncludePaths.AddRange(new string[] {
+			"MintChoco",
+			"MintChoco/Variant_Platforming",
+			"MintChoco/Variant_Platforming/Animation",
+			"MintChoco/Variant_Combat",
+			"MintChoco/Variant_Combat/AI",
+			"MintChoco/Variant_Combat/Animation",
+			"MintChoco/Variant_Combat/Gameplay",
+			"MintChoco/Variant_Combat/Interfaces",
+			"MintChoco/Variant_Combat/UI",
+			"MintChoco/Variant_SideScrolling",
+			"MintChoco/Variant_SideScrolling/AI",
+			"MintChoco/Variant_SideScrolling/Gameplay",
+			"MintChoco/Variant_SideScrolling/Interfaces",
+			"MintChoco/Variant_SideScrolling/UI"
+		});
+
+		// Uncomment if you are using Slate UI
+		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
+
+		// Uncomment if you are using online features
+		// PrivateDependencyModuleNames.Add("OnlineSubsystem");
+
+		// To include OnlineSubsystemSteam, add it to the plugins section in your uproject file with the Enabled attribute set to true
+	}
+}
---
+++ Source/MintChoco/MintChoco.cpp
@@ -0,0 +1,8 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "MintChoco.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, MintChoco, "MintChoco" );
+
+DEFINE_LOG_CATEGORY(LogMintChoco)
