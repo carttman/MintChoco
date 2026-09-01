68079fabbab20afd04dfb3dbeaca6377683dd9d5
carttman <93562297+carttman@users.noreply.github.com>
2026-09-01 12:28:15 +0900

    Add RoomItem Information struct

--- Source/MintChoco/Room/RoomItemWidget.cpp
+++ Source/MintChoco/Room/RoomItemWidget.cpp
@@ -14,9 +14,16 @@ void URoomItemWidget::SetInfo(FBlueprintSessionResult InSessionResult)
 
 void URoomItemWidget::RefreshUI()
 {
-	Txt_RoomName->SetText(FText());
+	FString RoomName;
+	Result.OnlineResult.Session.SessionSettings.Get(FName("ROOM_NAME"), OUT RoomName);
+	Txt_RoomName->SetText(FText::FromString(RoomName));
 
-	Txt_MapName->SetText(FText());
+	FString MapName;
+	Result.OnlineResult.Session.SessionSettings.Get(FName("MAP_NAME"), OUT MapName);
+	Txt_MapName->SetText(FText::FromString(MapName));
 
-	Txt_PlayerCount->SetText(FText());
+	const int32 CurrentPlayers = Result.OnlineResult.Session.SessionSettings.NumPublicConnections - Result.OnlineResult.Session.NumOpenPublicConnections;
+	const int32 MaxPlayers = Result.OnlineResult.Session.SessionSettings.NumPublicConnections;
+	FString PlayerCountString = FString::Printf(TEXT("( %d / %d )"), CurrentPlayers, MaxPlayers);
+	Txt_PlayerCount->SetText(FText::FromString(PlayerCountString));
 }
