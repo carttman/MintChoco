635b729c5b59e18e0c563172ee8e986c47c10cea
carttman <93562297+carttman@users.noreply.github.com>
2026-09-04 16:58:51 +0900

    Unit Base 클래스 추가

---
+++ Source/MintChoco/Game/ChocoUnit.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Game/Unit.h"
+#include "ChocoUnit.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MINTCHOCO_API AChocoUnit : public AUnit
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/MintChoco/Game/MintUnit.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Game/Unit.h"
+#include "MintUnit.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MINTCHOCO_API AMintUnit : public AUnit
+{
+	GENERATED_BODY()
+	
+};
