edcf7d4e0ea6b6339aab5caff081bdaddfafff2f
carttman <93562297+carttman@users.noreply.github.com>
2026-09-04 16:01:37 +0900

    Lobby to Game : 플레이어 닉네임, 팀 정보 전달

---
+++ Source/MintChoco/Game/GamePlayerState.h
@@ -0,0 +1,25 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerState.h"
+#include "GamePlayerState.generated.h"
+
+/**
+ *
+ */
+UCLASS()
+class MINTCHOCO_API AGamePlayerState : public APlayerState
+{
+	GENERATED_BODY()
+
+	virtual void GetLifetimeReplicatedProps(TArray< FLifetimeProperty >& OutLifetimeProps) const override;
+
+protected:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Replicated)
+	FText Nickname;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Replicated)
+	int32 Team;
+};
