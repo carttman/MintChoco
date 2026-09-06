ba605a9bc897ceb087b5eed258de175f7653ff6b
carttman <chj990920@gmail.com>
2026-09-06 14:30:54 +0900

    LobbyUserWidget : 로비 입장시 닉네임 늦게 갱신되는 버그 수정

--- Source/MintChoco/Lobby/Contents/LobbyGameMode.h
+++ Source/MintChoco/Lobby/Contents/LobbyGameMode.h
@@ -15,6 +15,14 @@ class MINTCHOCO_API ALobbyGameMode : public AGameModeBase
 	GENERATED_BODY()
 
 public:
+	/**
+	 * PlayerState가 처음 복제되기 전에 닉네임을 채워 넣는다.
+	 *
+	 * 클라이언트가 RPC로 알려주기를 기다리면 왕복이 한 번 더 들어가, 이름이 빈 칸으로
+	 * 먼저 보였다가 뒤늦게 채워진다.
+	 */
+	virtual void PostLogin(APlayerController* NewPlayer) override;
+
 	UFUNCTION(BlueprintImplementableEvent)
 	void BP_TryStartGame();
 
