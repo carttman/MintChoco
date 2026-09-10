81209e383a18a946f5bd8d0c25bee49e52a9d3a7
carttman <93562297+carttman@users.noreply.github.com>
2026-09-10 10:51:28 +0900

    캐릭터 애니메이션 추가, 방 생성 예외 처리 추가

--- Source/MintChoco/Room/CreateRoomPopupWidget.h
+++ Source/MintChoco/Room/CreateRoomPopupWidget.h
@@ -7,7 +7,9 @@
 #include "CreateRoomPopupWidget.generated.h"
 
 /**
+ * 방 만들기 팝업. 제목을 입력해야 만들기 버튼이 눌린다.
  *
+ * 버튼은 숨기지 않고 비활성화만 한다: 자리는 그대로 있고 회색으로 보인다.
  */
 UCLASS()
 class MINTCHOCO_API UCreateRoomPopupWidget : public UUserWidget
@@ -30,4 +32,13 @@ public:
 	UFUNCTION()
 	void OnCreateRoom();
 
+	/** 공백만 있는 제목은 빈 제목이다. */
+	static bool IsValidRoomName(const FText& Name);
+
+private:
+	UFUNCTION()
+	void OnRoomNameChanged(const FText& Text);
+
+	/** 제목이 유효할 때만 만들기 버튼을 켠다. 보이기는 건드리지 않는다. */
+	void UpdateCreateButton();
 };
