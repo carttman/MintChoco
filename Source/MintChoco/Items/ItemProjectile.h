97643cd91bcf25dc50e66262e4c7e6ab77707f1b
carttman <93562297+carttman@users.noreply.github.com>
2026-09-16 12:22:32 +0900

    사운드 에셋 추가, 꿀풍선 이펙트 수정중, 사운드 추가중

--- Source/MintChoco/Items/ItemProjectile.h
+++ Source/MintChoco/Items/ItemProjectile.h
@@ -8,6 +8,8 @@
 #include "ItemProjectile.generated.h"
 
 class AUnit;
+class UNiagaraComponent;
+class UNiagaraSystem;
 class UProjectileMovementComponent;
 class USphereComponent;
 class UStaticMeshComponent;
@@ -50,6 +52,12 @@ public:
 	/** 충돌 구의 반지름(cm). 궤적 미리보기가 같은 굵기로 훑는다. CDO에서도 읽을 수 있다. */
 	float GetCollisionRadius() const;
 
+	/**
+	 * 이 머신이 이 이펙트를 그려야 하는가. 데디케이티드 서버는 그림을 그리지 않고, 슬롯이 비어
+	 * 있으면 어느 머신에서도 아무 일이 없다. 판단만 하므로 월드 없이도 부를 수 있다.
+	 */
+	static bool ShouldShowFX(ENetMode NetMode, const UNiagaraSystem* FX);
+
 protected:
 	/** 서버 전용. 월드(벽, 바닥)에 막혔다. 기본은 터진다. */
 	virtual void HandleWorldHit(const FHitResult& Hit);
@@ -72,6 +80,23 @@ protected:
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Item")
 	TObjectPtr<UProjectileMovementComponent> Movement;
 
+	/**
+	 * 날아가는 동안 뒤에 남는 이펙트. 비워 두면 트레일이 없다(꿀벌이 그렇다).
+	 *
+	 * 프로필이 아니라 여기 두는 이유: 프로필은 서버만 받는다(SetProfile). 클라이언트 복사본은
+	 * 프로필이 없으므로 프로필에 넣으면 트레일이 리슨 호스트 화면에만 보인다. 클래스 디폴트는
+	 * 모든 머신이 갖고 있다.
+	 */
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Item")
+	TObjectPtr<UNiagaraSystem> TrailFX;
+
+	/**
+	 * 공 자체에 얹히는 이펙트. 뒤로 끌리는 트레일과 달리 메시를 감싸고 같이 날아간다. 비워 두면
+	 * 없다. 트레일과 같은 자리(메시 원점)에 붙으므로 둘을 같이 켜도 어긋나지 않는다.
+	 */
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Item")
+	TObjectPtr<UNiagaraSystem> BodyFX;
+
 	/** 던진 팀. 초기 복제. */
 	UPROPERTY(VisibleInstanceOnly, Replicated, Category = "Item")
 	int32 Team = Teams::None;
@@ -83,6 +108,12 @@ protected:
 	bool bDetonated = false;
 
 private:
+	/**
+	 * 슬롯이 찼고 이 머신이 그리는 머신이면, 메시에 붙여 팀색을 넣어 띄운다. 그릴 필요가 없으면
+	 * 아무 일도 하지 않는다. 트레일과 바디가 같은 처리를 탄다.
+	 */
+	void SpawnAttachedFX(UNiagaraSystem* FX);
+
 	UFUNCTION()
 	void OnSphereHit(UPrimitiveComponent* HitComponent, AActor* OtherActor, UPrimitiveComponent* OtherComponent,
 		FVector NormalImpulse, const FHitResult& Hit);
